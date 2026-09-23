54ce227f9c3d8ba49ea77f0b7f6215e0dba03913
love, nebula <nejinekun@gmail.com>
2026-09-23 11:55:10 -0400

    Initializing
    Commit #1 From USB to Git

---
+++ WORKSPACE/Assignment1/Assignment1/Employee.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** STATEMENT OF AUTHORSHIP
+ * 
+ * I, 000964569, Neil Patrick Olaires hereby declare that this
+ * is my own work and i have not shared this work with anyone 
+ * or have even used ai for this program
+ */
+
+namespace Assignment1
+{
+    internal class Employee
+    {
+        // ATTRIBUTES
+
+
+        /// <summary>
+        /// Employee name
+        /// </summary>
+        private string name;
+        /// <summary>
+        ///  Employee number
+        /// </summary>
+        private int number;
+        /// <summary>
+        /// Employee rate, pay
+        /// </summary>
+        private decimal rate;
+        /// <summary>
+        /// Number of hours employee works -> this is a double
+        /// </summary>
+        private double hours;
+
+
+
+
+
+        //CONSTRUCTOR
+
+        /// <summary>
+        /// Constructor for employee
+        /// 
+        /// </summary>
+        /// <param name="name"> name of the employee</param>
+        /// <param name="number"> the employee id number</param>
+        /// <param name="rate"> rate of their pay </param>
+        /// <param name="hours">number of hours an employee works</param>
+        public Employee(string name, int number, decimal rate, double hours)
+        {
+            this.name = name;
+            if (number < 0)
+            {
+                this.number = number; //id cant be negative 
+            }
+            if (rate < 0) { 
+                this.rate = rate; // rate cant be negative
+            }
+            if (hours < 0)
+            {
+                this.hours = hours; //hours cant be negative
+            }
+        }
+
+        //GETTERS AND SETTERS 
+        public double GetHours(){return this.hours;}
+        public string GetName(){return this.name;}
+        public decimal GetRate(){return this.rate;}
+
+        public decimal GetGross()
+        {
+            if (this.hours > 40) // in the case of overtime
+            {
+                decimal overtimeHours = (decimal)(this.hours - 40)*(decimal)1.5; //store time and a half hours
+
+                decimal normalGross = (decimal)this.hours * this.rate;
+                decimal excessGross = overtimeHours * this.rate;
+                return normalGross + excessGross;               //calculate normal gross and add excess gross for full gross
+
+
+            } else               //under 40 hours
+            {
+                return this.rate * (decimal)this.hours;
+            }
+        }
+
+        public void SetHours(double hours){this.hours = hours;}
+        public void SetName(string name){this.name = name;}
+        public void SetNumber(int number){this.number = number;}
+        public void SetRate(decimal rate){this.rate = rate;}
+
+
+
+
+
+
+
+
+
+
+        //TO STRING
+        public override string ToString()
+        {
+            return "this is a normal to string";
+        }
+
+
+
+    }
+}
---
+++ WORKSPACE/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/** STATEMENT OF AUTHORSHIP
+ * 
+ * I, 000964569, Neil Patrick Olaires hereby declare that this
+ * is my own work and i have not shared this work with anyone 
+ * or have even used ai for this program
+ */
+
+namespace Assignment1
+{
+    
+    internal class Program
+    {
+        /// <summary>
+        /// STORED employee.txt PATH INTO STRING
+        /// csv -> comma seperated values
+        /// </summary>
+        const string DATAFILE = "employees.txt";
+
+        /// <summary>
+        /// INITIALIZED EMPLOYEE ARRAY 
+        /// 
+        /// 1. new size reefined every entry-consoleclear
+        /// </summary>
+        Employee[] employeeList;
+
+
+        /// <summary>
+        /// MAIN METHOD -> VIEW CLASS
+        /// 
+        /// Program method -> association with Employee class
+        /// (Program has * employees)
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+
+
+
+
+
+            //RUNNING PROGRAM
+            bool running = true;
+            while (running)
+            {
+                Console.Clear();//clears console
+                Console.WriteLine("Select a menu item:" +
+                    "\n[A] Sort by employee name \t(asc)" +
+                    "\n[B] Sort by employee number \t(asc)" +
+                    "\n[C] Sort by employee pay rate \t(desc)" +
+                    "\n[D] Sort by employee hours \t(desc)" +
+                    "\n[E] Sort by employee gross pay\t (desc)" +
+                    "\n[X] Exit");
+
+                string option = Console.ReadLine();
+
+                switch (option)
+                {
+                    //SORT NAME
+                    case ("A"):
+                        break;
+
+                    //SORT NUMBER
+                    case ("B"):
+                        break;
+
+                    //SORT PAYRATE
+                    case ("C"):
+                        break;
+
+                    //SORT HOURS
+                    case ("D"):
+                        break;
+
+                    //SORT GROSS PAY
+                    case ("E"):
+                        break;
+
+                    //EXIT
+                    case ("X"): 
+                        running = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input... ");
+                        break;
+
+                }
+
+                Console.WriteLine("\nPress any key to continue...");
+            }//END OF WHILE LOOP
+
+            Console.WriteLine("Have a nice day!");
+
+
+        }
+
+
+
+
+
+
+
+        /// <summary>
+        /// READ METHOD TO COUNT AND INSERT EMPLOYEES INTO ARRAY
+        /// ***has exception checking (try catch)
+        /// 
+        /// </summary>
+        public void readEmployees()
+        {
+            try {
+                StreamReader reader = new StreamReader(DATAFILE);
+                while(!reader.EndOfStream)
+                {
+                    //columnAttribute = reader.ReadLine().Split(',');
+
+
+
+                }
+
+
+
+            } catch (Exception ex)
+            {
+                Console.Error.WriteLine("Error reading the file: " + ex.Message);
+            }
+
+        }
+
+        public void readSize()
+        {
+            try
+            {
+
+            }catch(Exception ex)
+            {
+                Console.Error.WriteLine("Error reading the file: " + ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// PRINTS THE ARRAY OF EMPLOYEES CREATED BY THE READ METHOD
+        /// </summary>
+        public void printEmployees()
+        {
+
+        }
+    }
+}
